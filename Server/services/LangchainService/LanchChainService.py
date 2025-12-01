bfc09fb6742fefbd74a0b97225e8c749c9ae465c
Varun Harinath <varunharinath15@gmail.com>
2025-12-01 13:05:39 -0600

    Updated: LangchainService for Step_1-Prompt

--- Server/services/LangchainService/LanchChainService.py
+++ Server/services/LangchainService/LanchChainService.py
@@ -1,25 +1,18 @@
 from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAI
-from langchain.agents import create_agent,AgentState
-from langchain.messages import HumanMessage
-from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from schemas.BaseModelSchemas import ErrorBaseModel,RequestBaseModel
 from .tools.LangchainAgentTools import AdditionTool
-from langchain.tools import tool
 from langchain_core.prompts import ChatPromptTemplate
 from .prompts.BasePrompts import STEP_1_PROMPT,STEP_2_PROMPT
 import os
 import re
 import json
-
 load_dotenv()
-
-
 class Langchain_Service:
     def __init__(self):
         self.tools = [AdditionTool]
-        self.llm = GoogleGenerativeAI(model = os.getenv('GOOGLE_AI_MODEL'))
-        
+        self.llm = GoogleGenerativeAI(model = os.getenv('GOOGLE_AI_MODEL'))        
     @staticmethod    
     def parse_llm_json(raw_text: str):
         if not raw_text:
@@ -28,13 +21,10 @@ class Langchain_Service:
         if cleaned.startswith('"') and cleaned.endswith('"'):
             cleaned = cleaned[1:-1]
             cleaned = cleaned.encode("utf-8").decode("unicode_escape")
-
         try:
             return json.loads(cleaned)
         except json.JSONDecodeError:
             return {"raw_output": cleaned}
-        
-    
     def prompt_invoke(self,query,prompt):
         prompt = ChatPromptTemplate.from_messages([
             ("system",prompt),
@@ -43,7 +33,6 @@ class Langchain_Service:
         chain = prompt | self.llm
         output = chain.invoke({"user_message":query})
         return self.parse_llm_json(output)
-    
     def invoke_agent(self,query:RequestBaseModel,mode):
         try:
             if mode == 'step_1':
@@ -54,7 +43,6 @@ class Langchain_Service:
                 return ErrorBaseModel(success=False,error="TypeError: Mode not specified properly. It's either 'step_1' or 'step_2' ")
         except Exception as e:
             return ErrorBaseModel(success=False,error=str(e))
-    
 
         
     
