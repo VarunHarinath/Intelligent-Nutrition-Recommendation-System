c6db90029f9c318cf17a4ebe5754f58f66b9897e
Varun Harinath <varunharinath15@gmail.com>
2025-12-05 16:20:23 -0600

    Updated: Client & Server Integration

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from routes.DataInputRoute import DataInputRoute
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Intelligent-Nutrition-Recommendation-System")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 def home():
